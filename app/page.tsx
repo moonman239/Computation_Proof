b57c2c19892c35a1435df804fa2a363bd2b60cba
Montana Burr <bur18019@byui.edu>
2023-09-21 15:00:26 -0600

    sends nothing, gets response

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,38 @@
+"use client";  // since state is used, we can't use a server-based component
 import Image from 'next/image'
+import { ChangeEvent, FormEvent, useState } from 'react';
+import {ResponseJSON } from "./circom/types";
+import { ConfirmCustomerBalancePaymentData } from '@stripe/stripe-js';
 
 export default function Home() {
+  const [files,setFiles] = useState<FileList | null>();
+  const onSubmit = (e:FormEvent)=>{
+    e.preventDefault();
+    if (files)
+       fetch("circom",{
+          headers: {"Content-Type":"application/json"},
+          method: "POST",
+          body: JSON.stringify(files)
+        }).then((response)=>response.json()).then((json: ResponseJSON)=>{
+          alert(JSON.stringify(json));
+        }).catch((e)=>console.error("Response error:" + e))
+    else
+      alert("Please add files.");
+  }
+  const fileChange = (event:ChangeEvent<HTMLInputElement>)=>
+  {
+    const _files = event.target.files;
+    setFiles(_files);
+  }
   return (
     <div>
-      <h3>I can prove that the solution to x**2 = 4 is +/- 2</h3>
-      <a href="/verifier">click to see verifier</a>
+      <h3>Circuit Prover</h3>
+      Select a circum file, then click Upload to prove it.
+      <form onSubmit={onSubmit}>
+      <input type="file" accept=".circom" onChange={fileChange}></input>
+      <button type="submit">Upload</button>
+      </form>
+      <div></div>
     </div>
   )
 }
