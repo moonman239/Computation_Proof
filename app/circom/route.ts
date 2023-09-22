6933a07885d905e95670c9aac26f13de8a80a4f3
Montana Burr <bur18019@byui.edu>
2023-09-22 14:39:01 -0600

    added logging of buffer.error, fixed bug

--- app/circom/route.ts
+++ app/circom/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import * as types from "./types";
 const { v4: uuidv4 } = require('uuid');
-const {exec} = require("child_process");
+import {spawnSync} from "child_process";
 import * as fs from "fs";
 
 export async function POST(req: Request)
@@ -10,28 +10,46 @@ export async function POST(req: Request)
     req.body
     const values = formData.values();
     let row = values.next();
-    const badBlobs: Array<Blob | undefined> = [];
+    const errors = new Array<{blob: Blob,error: string}>();
+    const compiledCircuits = new Array<string>();
     while (!row.done)
     {
         const uuid = uuidv4();
         const inputFilePath = uuid + ".circom";
         const outputFilePath = uuid + ".json";
-        const blob: Blob | undefined = row.value;
+        const blob = (row.value as Blob);
+        const value = await blob.text();
+        fs.writeFileSync(inputFilePath,value,{
+        flag:"w"
+        });
+        fs.writeFileSync(outputFilePath,"",{
+        flag:"w"
+        });
+        const buffer = spawnSync("circom",[inputFilePath,"-o",outputFilePath],{
+            stdio:["inherit","pipe","pipe"]
+        });
 
-        if (blob)
+        if (buffer.error)
+            console.error("circom buffer error" + buffer.error);
+        const cmdOut = buffer.stdout;
+        if (cmdOut)
+            console.log(cmdOut.toString());
+        console.log("circom out: " + cmdOut);
+        const cmdError = buffer.stderr;
+        if (cmdError)
         {
-            const value = await blob.text();
-            fs.writeFileSync(inputFilePath,value,{
-            flag:"w"
-            });
-            exec("npx circom " + inputFilePath + " -o " + outputFilePath);
+            const errorString = cmdError.toString();
+            console.error("circom error:" + errorString);
+            errors.push({blob: blob,error:errorString});
         }
         else
         {
-            badBlobs.push(blob);
-            console.error("undefined blob");
+            // send file in response
+            const fileBuffer = fs.readFileSync(outputFilePath);
+            const compiledCircuit = fileBuffer.toString();
+            compiledCircuits.push(compiledCircuit);
         }
         row = values.next();
     }
-    return NextResponse.json({badBlobs: badBlobs});
+    return NextResponse.json({compiledCircuits:compiledCircuits, errors:errors});
 }
