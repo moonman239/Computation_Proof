985f36bd36a6b2319261f4d48ed4908e35114f53
Montana Burr <bur18019@byui.edu>
2023-10-05 11:16:57 -0600

    displays errors generated by circom

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,7 @@ export default function Home() {
     if (files)
     {
         compileCircuit(files[0]).then(()=>setSuccess(true)).catch((e)=>{
-          alert("Could not compile file. Please check your program and try again later.");
+          alert("Compilation error: " + e);
           console.error("Response error:" + e);
           setSuccess(false);
       })
